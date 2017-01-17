62d623c8cfdf64c2ab434d4d6edc85965720a7fd
Richard Ankers <richardankers@gmail.com>
2017-01-17 15:31:38 +0000

    Add certs folder

--- push-notification-tester/index.js
+++ push-notification-tester/index.js
@@ -1,19 +1,21 @@
-var apn = require('apn');
+const apn = require('apn');
 
-var options = {
-  cert: "",
-  key: "",
+const options = {
+  cert: "certs/dev_con_key&cert.pem",
+  key: "certs/dev_con_key&cert.pem",
   production: false,
 };
 
-let deviceTokens = [""];
+const deviceTokens = [""];
 
-var apnProvider = new apn.Provider(options);
+const apnProvider = new apn.Provider(options);
 
-let notification = new apn.Notification();
+const notification = new apn.Notification();
 notification.alert = "Hello, world!";
 notification.badge = 1;
 notification.topic = "";
+notification.payload = {"event_type": "customer_survey"};
+
 
 apnProvider.send(notification, deviceTokens).then( (response) => {
   if (response.failed) {
